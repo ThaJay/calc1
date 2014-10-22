a280fc795c6bc1b1271165046d2402c398b452d7
ThaJay <jwtikbendj@gmail.com>
2014-10-22 02:45:52 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
+    
     //er voor zorgen dat de pagina onder de navbar begint (werkt nog niet met verticale navbar(kleine schermen))
     $(document.body).css('padding-top', $('#topnavbalk').height() + 10);
 	$(window).resize(function(){
 		$(document.body).css('padding-top', $('#topnavbalk').height() + 10);
 	});
+    
     //blokken per rij gelijke hoogte maken. Is nu nog per rij gespecificeerd dus vereist nog een aanpassing.
 	$('#row1 .block').css({
 		'height': $('#row1 .block').parent().height()
@@ -11,15 +13,20 @@ $(document).ready(function () {
 	$('#row2 .block').css({
 		'height': $('#row2 .block').parent().height()
 	});
+    
+    
     //hier komt de calculator logica
     //calculator var
-	var ans             = $("#antwoord"),
+    var ans             = $("#antwoord"),
+        is              = $("#is"),
+        reset           = $("#c"),
 	    calcField       = "0",
         isPressed       = false,
         rst             = function () {
             calcField = "0";
             ans.text(calcField);
-        };
+        },
+    
     //knopjes drukken functie
 	function Knop(jQ,autoReset) {
         jQ.click(function () {
@@ -35,6 +42,7 @@ $(document).ready(function () {
 	        isPressed = false;
 	    });
 	}
+    
     //knopjesss
 	$(".numberBtn").each(function () {
         Knop($(this),true)
@@ -42,11 +50,20 @@ $(document).ready(function () {
 	$(".functionBtn").each(function () {
         Knop($(this),false)
 	});
-    var is      = $("#is"),
-        reset   = $("#c")
+    
+    //reset
+	reset.click(function () {
+	    rst()
+	});
+    
     //Rekenen maar!
     is.click(function () {
-        if (calcField[0] === "*" | calcField[0] === "/" | Boolean(calcField.match(/[+\-/*]{2,}/g)) | Boolean(calcField.match(/[+\-/*][.]+[+\-/*]/g)) | Boolean(calcField.match(/[.]{2,}/g))) {
+        if (
+            calcField[0] === /[*/]/g |
+            Boolean(calcField.match(/[+\-/*]{2,}/g)) |
+            Boolean(calcField.match(/[+\-/*][.]+[+\-/*]/g)) |
+            Boolean(calcField.match(/[.]{2,}/g))
+            ){
             ans.text("invalid");
             isPressed = true;
         }
@@ -56,9 +73,5 @@ $(document).ready(function () {
             ans.text(calcField);
             isPressed = true;
         };
-    });
-    //reset
-    reset.click(function () {
-        rst()
     });
 });
